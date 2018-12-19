3b4ca5a1315d912adee8ec6fc57b4022ab883cbc
chulei <1062186165@qq.com>
2018-12-19 10:20:05 +0800

    restconfig not exit

--- console/pkg/controller/deployconfig_test.go
+++ console/pkg/controller/deployconfig_test.go
@@ -5,14 +5,19 @@ import (
 	"hidevops.io/hiboot/pkg/log"
 	"hidevops.io/hiboot/pkg/utils/io"
 	"hidevops.io/mio/console/pkg/aggregate/mocks"
+	builder "hidevops.io/mio/console/pkg/builder/mocks"
 	"hidevops.io/mio/console/pkg/command"
+	"hidevops.io/mio/pkg/apis/mio/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"net/http"
 	"testing"
 )
 
 func TestDeploymentConfig(t *testing.T) {
 	deploy := new(mocks.DeploymentConfigAggregate)
-	appInfo := newDeploymentConfigController(deploy, nil)
+	deployment := new(builder.DeploymentConfigBuilder)
+	appInfo := newDeploymentConfigController(deploy, deployment)
 	deploy.On("Create", "", "", "", "", "", "", "dev").Return(nil, nil)
 
 	app := web.NewTestApp(t, appInfo).
@@ -22,5 +27,32 @@ func TestDeploymentConfig(t *testing.T) {
 	log.Println(io.GetWorkDir())
 	t.Run("should pass with jwt token", func(t *testing.T) {
 		app.Post("/deploymentConfig/create").WithJSON(&command.DeployConfigType{}).Expect().Status(http.StatusOK)
+	})
+	ds := &v1alpha1.Deployment{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "hello-world",
+			Namespace: "demo",
+		},
+		Spec: v1alpha1.DeploymentConfigSpec{
+			Version: "v1",
+			Labels: map[string]string{
+				"app":     "hello-world",
+				"version": "v1",
+			},
+			Port: []corev1.ContainerPort{
+				corev1.ContainerPort{
+					ContainerPort: 8080,
+					Protocol:      "TCP",
+				},
+				corev1.ContainerPort{
+					ContainerPort: 7575,
+					Protocol:      "TCP",
+				},
+			},
+		},
+	}
+	deployment.On("CreateApp", ds).Return(nil)
+	t.Run("should pass with jwt token", func(t *testing.T) {
+		app.Post("/deploymentConfig/app").WithJSON(&App{}).Expect().Status(http.StatusOK)
 	})
 }
