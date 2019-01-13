859f5acb564e7ccb035eb66a34a75c93194f1a08
chulei <1062186165@qq.com>
2019-01-13 23:12:59 +0800

    hiadmin to mio-console

--- console/build.sh
+++ console/build.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 echo  "GOOS=linux go build"
- GOOS=linux go build -o hiadmin
+ GOOS=linux go build -o mio-console
 
-docker build -t hiadmin .
-
-docker tag hiadmin docker-registry-default.app.vpclub.io/demo/hiadmin:v1
+docker build -t docker-registry-default.app.vpclub.io/hidevopsio/mio-console:v1 .
 
 docker login -p $(oc whoami -t) -u unused docker-registry-default.app.vpclub.io
 
-docker push docker-registry-default.app.vpclub.io/demo/hiadmin:v1
+docker push docker-registry-default.app.vpclub.io/hidevopsio/mio-console:v1
 
 
