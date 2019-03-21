f3fbf3b6b01733442d288adfb3aa9a5e325b7389
ClareChu <1062186165@qq.com>
2019-03-21 20:27:28 +0800

    branch

--- console/build-docker-hub.sh
+++ console/build-docker-hub.sh
@@ -3,8 +3,8 @@
 echo  "GOOS=linux go build"
  GOOS=linux go build -o mio-console
 
-docker build -t hidevops/mio-console:v1.0.1 .
+docker build -t hidevops/mio-console:v1.0.2 .
 
-docker push hidevops/mio-console:v1.0.1
+docker push hidevops/mio-console:v1.0.2
 
 
