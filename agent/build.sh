838fecd1caea39beb3d6a5b8a77173da2c0e55f0
ClareChu <102186165@qq.com>
2019-06-04 20:09:39 +0800

    docker build agent

---
+++ agent/build.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# build
+
+##
+
+export GROUP=hidevops
+export BINARY=agent
+export TAG=v1.0.8
+
+
+GOOS=linux go build -o agent
+
+## java name tag
+
+export JAVA_IMAGE_NAME=agent-java-jar
+
+## go name tag
+
+export GO_IMAGE_NAME=agent-go
+
+## docker login java
+
+docker login -u admin -p Harbor12345 harbor.cloud2go.cn
+
+## docker build go
+
+cp docker/java/Dockerfile  Dockerfile
+
+docker build -t hidevops/agent-java-jar:v1.0.8 .
+
+rm -rf Dockerfile
+
+docker push hidevops/agent-java-jar:v1.0.8
+
+## docker build go
+
+cp docker/go/Dockerfile  Dockerfile
+
+docker build -t hidevops/agent-go:v1.0.8 .
+
+rm -rf Dockerfile
+
+docker push hidevops/agent-go:v1.0.8
+
+
+rm -rf agent
